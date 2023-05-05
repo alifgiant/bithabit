44eb2741de982cb3dcd0c3cdcf4f05af3363299e
alifgiant <personal@alifakbar.com>
2023-05-05 20:24:52 +0700

    initial structure

---
+++ lib/src/model/habit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../res/res_color.dart';
+
+class Habit {
+  final String name;
+  final HabitColor color;
+  final DateTime? reminder;
+
+  Habit(this.name, this.color, this.reminder);
+}
+
+enum HabitColor {
+  purple(ResColor.purple, ResColor.black),
+  yellow(ResColor.yellow, ResColor.black),
+  pink(ResColor.pink, ResColor.black),
+  darkGreen(ResColor.darkGreen, ResColor.black),
+  lightGreen(ResColor.lightGreen, ResColor.black),
+  darkBlue(ResColor.darkBlue, ResColor.black),
+  lightBlue(ResColor.lightBlue, ResColor.black);
+
+  const HabitColor(this.mainColor, this.textColor);
+  final Color mainColor;
+  final Color textColor;
+}
---
+++ lib/src/res/res_color.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+mixin ResColor {
+  static const purple = Color(0xffFFFFFF);
+  static const yellow = Color(0xffFCF6B1);
+  static const pink = Color(0xffFF7B9C);
+  static const darkGreen = Color(0xff0CBABA);
+  static const lightGreen = Color(0xffA9E5BB);
+  static const darkBlue = Color(0xff01BAEF);
+  static const lightBlue = Color(0xff58FCEC);
+
+  // base
+  static const black = Color(0xff172121);
+  static const white = Color(0xffFCFCFC);
+}
