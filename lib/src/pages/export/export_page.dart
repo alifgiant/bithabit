4c7963de9b391c7e4dc757fc4ca2c7fd7d0c5375
alifgiant <personal@alifakbar.com>
2023-05-26 21:52:19 +0800

    handle error import message

--- lib/src/pages/export/export_page.dart
+++ lib/src/pages/export/export_page.dart
@@ -34,7 +34,7 @@ class ExportPage extends StatelessWidget {
               child: ElevatedButton(
                 onPressed: () async {
                   final scaffoldMessenger = ScaffoldMessenger.of(context);
-                  final file = await ExporterUtils().dumpFile(ImportExportData(
+                  final file = await ExporterUtils().dumpFile(BundleData(
                     [...enabledHabits, ...disabledHabits],
                     timelineService.timelineMap,
                   ));
@@ -60,25 +60,15 @@ class ExportPage extends StatelessWidget {
               child: ElevatedButton(
                 onPressed: () async {
                   final scaffoldMessenger = ScaffoldMessenger.of(context);
-                  final data = await ExporterUtils().importFile();
-                  if (data == null) return;
-
-                  bool habbitAdded = false;
-                  for (final habit in data.habits) {
-                    habitService.saveHabit(habit);
-
-                    final timeline = data.timelines[habit.id];
-                    if (timeline == null) continue;
-                    for (final time in timeline) {
-                      timelineService.check(habit, time, setAction: false);
-                    }
-
-                    habbitAdded = true;
-                  }
-
-                  scaffoldMessenger.showSnackBar(SnackBar(
-                    content: Text(habbitAdded ? 'Habit Successfully Imported' : 'Data File is Empty'),
-                  ));
+                  final colorScheme = Theme.of(context).colorScheme;
+                  final result = await ExporterUtils().importFile();
+                  handleImportResult(
+                    scaffoldMessenger,
+                    colorScheme,
+                    result,
+                    habitService,
+                    timelineService,
+                  );
                 },
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -95,6 +85,87 @@ class ExportPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  void handleImportResult(
+    ScaffoldMessengerState scaffold,
+    ColorScheme colorScheme,
+    ImportResult result,
+    HabitService habitService,
+    TimelineService timelineService,
+  ) {
+    if (result is ErrorImportResult) {
+      return handleErrorResult(scaffold, colorScheme, result, habitService, timelineService);
+    } else if (result is SuccessImportResult) {
+      return handleSuccessResult(scaffold, colorScheme, result, habitService, timelineService);
+    }
+  }
+
+  void handleErrorResult(
+    ScaffoldMessengerState scaffold,
+    ColorScheme colorScheme,
+    ErrorImportResult result,
+    HabitService habitService,
+    TimelineService timelineService,
+  ) {
+    switch (result.type) {
+      case ErrorType.fileCorrupt:
+        scaffold.showSnackBar(
+          SnackBar(
+            content: const Text('Data File is Corrupt'),
+            backgroundColor: colorScheme.error,
+          ),
+        );
+        break;
+      case ErrorType.tooMuchFile:
+        scaffold.showSnackBar(
+          SnackBar(
+            content: const Text('Please only select one file'),
+            backgroundColor: colorScheme.error,
+          ),
+        );
+        break;
+      case ErrorType.wrongFileFormat:
+        scaffold.showSnackBar(
+          SnackBar(
+            content: const Text("You're selecting wrong backup file"),
+            backgroundColor: colorScheme.error,
+          ),
+        );
+        break;
+      default:
+      // case ErrorType.cancel:
+    }
+  }
+
+  void handleSuccessResult(
+    ScaffoldMessengerState scaffold,
+    ColorScheme colorScheme,
+    SuccessImportResult result,
+    HabitService habitService,
+    TimelineService timelineService,
+  ) {
+    final data = result.data;
+    if (data.habits.isEmpty) {
+      scaffold.showSnackBar(
+        const SnackBar(content: Text('Data File is Empty')),
+      );
+    }
+
+    for (final habit in data.habits) {
+      habitService.saveHabit(habit);
+
+      final timeline = data.timelines[habit.id];
+      if (timeline == null) continue;
+
+      for (final time in timeline) {
+        timelineService.check(habit, time, setAction: false);
+      }
+    }
+
+    scaffold.showSnackBar(
+      const SnackBar(content: Text('Habit Successfully Imported')),
     );
   }
 }
--- lib/src/utils/exporter/exporter_utils.dart
+++ lib/src/utils/exporter/exporter_utils.dart
@@ -3,14 +3,15 @@ import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
+
 import '../../model/habit.dart';
 
 class ExporterUtils {
-  Future<File?> dumpFile(ImportExportData data) async {
+  Future<File?> dumpFile(BundleData data) async {
     String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
 
+    // User canceled the picker
     if (selectedDirectory == null) return null;
-    // final directory = await getApplicationDocumentsDirectory();
 
     // TODO: handle web file
     final file = File('$selectedDirectory/export-${DateTime.now().millisecondsSinceEpoch}.json');
@@ -29,43 +30,67 @@ class ExporterUtils {
     return file.writeAsString(jsonString);
   }
 
-  Future<ImportExportData?> importFile() async {
-    final result = await FilePicker.platform.pickFiles();
+  Future<ImportResult> importFile() async {
+    final result = await FilePicker.platform.pickFiles(
+      allowedExtensions: ['json'],
+    );
 
-    if (result != null && !kIsWeb) {
-      // result.files.first.
-      // print('path: ${result.files.single.path}');
-      final file = File(result.files.single.path!);
-      // final isExist = file.existsSync();
-      // print('isExist : $isExist');
-      final encondedData = await file.readAsString();
+    // User canceled the picker
+    if (result == null) return ErrorImportResult(ErrorType.cancel);
+    if (result.files.length > 1) return ErrorImportResult(ErrorType.tooMuchFile);
+    if (result.files.first.extension != 'json') return ErrorImportResult(ErrorType.wrongFileFormat);
 
-      final json = jsonDecode(encondedData);
-      final habits = (json['habits'] as List?)?.map((e) => Habit.fromJson(e)) ?? [];
-      Map<String, Set<DateTime>> timelines = {};
-      for (final element in (json['timeline'] as Map).entries) {
-        timelines[element.key] = (element.value as List)
-            .cast<int>()
-            .map(
-              (e) => DateTime.fromMillisecondsSinceEpoch(e),
-            )
-            .toSet();
-      }
-
-      return ImportExportData(habits, timelines);
-    } else if (kIsWeb) {
+    if (kIsWeb) {
       // TODO: handle web pick file
-      return null;
+      return ErrorImportResult(ErrorType.cancel);
     } else {
-      // User canceled the picker or
-      return null;
+      final file = File(result.files.first.path!);
+
+      // if file actually empty
+      final isExist = file.existsSync();
+      if (!isExist) return ErrorImportResult(ErrorType.fileCorrupt);
+
+      final encondedData = await file.readAsString();
+      try {
+        final json = jsonDecode(encondedData);
+        final habits = (json['habits'] as List?)?.map((e) => Habit.fromJson(e)) ?? [];
+        Map<String, Set<DateTime>> timelines = {};
+        for (final element in (json['timeline'] as Map).entries) {
+          timelines[element.key] = (element.value as List)
+              .cast<int>()
+              .map(
+                (e) => DateTime.fromMillisecondsSinceEpoch(e),
+              )
+              .toSet();
+        }
+
+        return SuccessImportResult(BundleData(habits, timelines));
+      } catch (_) {
+        return ErrorImportResult(ErrorType.fileCorrupt);
+      }
     }
   }
 }
 
-class ImportExportData {
+class BundleData {
   final Iterable<Habit> habits;
   final Map<String, Set<DateTime>> timelines;
 
-  ImportExportData(this.habits, this.timelines);
+  BundleData(this.habits, this.timelines);
+}
+
+abstract class ImportResult {}
+
+class ErrorImportResult extends ImportResult {
+  final ErrorType type;
+
+  ErrorImportResult(this.type);
+}
+
+enum ErrorType { cancel, tooMuchFile, wrongFileFormat, fileCorrupt }
+
+class SuccessImportResult extends ImportResult {
+  final BundleData data;
+
+  SuccessImportResult(this.data);
 }
