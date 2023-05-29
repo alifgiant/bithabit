9a719ffc638810682dc2352233eef1d119635b14
alifgiant <personal@alifakbar.com>
2023-05-29 16:31:14 +0800

    fix overview sizing

--- lib/src/pages/charts/overview_progress.dart
+++ lib/src/pages/charts/overview_progress.dart
@@ -21,8 +21,16 @@ class OverviewProgress extends StatelessWidget {
 
     final timelineService = context.watch<TimelineService>();
 
-    final prevMonthRatio = timelineService.habitCompletion(habit, today.year, month: today.month - 1);
-    final curMonthRatio = timelineService.habitCompletion(habit, today.year, month: today.month);
+    final prevMonthRatio = timelineService.habitCompletion(
+      habit,
+      today.year,
+      month: today.month - 1,
+    );
+    final curMonthRatio = timelineService.habitCompletion(
+      habit,
+      today.year,
+      month: today.month,
+    );
     final monthDiff = curMonthRatio - prevMonthRatio;
 
     final prevYearRatio = timelineService.habitCompletion(habit, today.year - 1);
@@ -31,49 +39,61 @@ class OverviewProgress extends StatelessWidget {
 
     final bestStreak = timelineService.countBestStreak(habit);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          _DetailOverview(
-            habit: habit,
-            value: curMonthRatio.toString(),
-            text: 'Current\nMonth',
-            mode: _OverviewMode.circleProgress,
-          ),
-          _DetailOverview(
-            habit: habit,
-            value: monthDiff.toPercentage(),
-            text: 'Prev\nMonth',
-            mode: _OverviewMode.text,
-          ),
-          _DetailOverview(
-            habit: habit,
-            value: yearDiff.toPercentage(),
-            text: 'Prev\nYear',
-            mode: _OverviewMode.text,
-          ),
-          _DetailOverview(
-            habit: habit,
-            value: bestStreak.formatShort(),
-            text: 'Best\nStreak',
-            mode: _OverviewMode.text,
-          ),
-        ],
-      ),
-    );
+    const paddingH = 12.0;
+    const spaceH = 6.0;
+
+    return LayoutBuilder(builder: (ctx, cons) {
+      final maxWidth = cons.maxWidth - paddingH * 2 - spaceH * 3;
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: paddingH),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _DetailOverview(
+              width: maxWidth / 4,
+              bgColor: habit.color.mainColor.withOpacity(0.2),
+              value: curMonthRatio.toString(),
+              text: 'Current\nMonth',
+              mode: _OverviewMode.circleProgress,
+            ),
+            _DetailOverview(
+              width: maxWidth / 4,
+              bgColor: habit.color.mainColor.withOpacity(0.2),
+              value: monthDiff.toPercentage(),
+              text: 'Prev\nMonth',
+              mode: _OverviewMode.text,
+            ),
+            _DetailOverview(
+              width: maxWidth / 4,
+              bgColor: habit.color.mainColor.withOpacity(0.2),
+              value: yearDiff.toPercentage(),
+              text: 'Prev\nYear',
+              mode: _OverviewMode.text,
+            ),
+            _DetailOverview(
+              width: maxWidth / 4,
+              bgColor: habit.color.mainColor.withOpacity(0.2),
+              value: bestStreak.formatShort(),
+              text: 'Best\nStreak',
+              mode: _OverviewMode.text,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
 
 class _DetailOverview extends StatelessWidget {
-  final Habit habit;
+  final double width;
+  final Color bgColor;
   final String value;
   final String text;
   final _OverviewMode mode;
 
   const _DetailOverview({
-    required this.habit,
+    required this.width,
+    required this.bgColor,
     required this.value,
     required this.text,
     required this.mode,
@@ -103,8 +123,8 @@ class _DetailOverview extends StatelessWidget {
         return Center(
           child: Text(
             value,
-            style: const TextStyle(
-              fontSize: 21,
+            style: TextStyle(
+              fontSize: width / 4 - 2,
               fontWeight: FontWeight.w600,
             ),
           ),
@@ -113,24 +133,26 @@ class _DetailOverview extends StatelessWidget {
     }
 
     return Container(
+      width: width,
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 2),
       decoration: BoxDecoration(
-        color: habit.color.mainColor.withOpacity(0.2),
+        color: bgColor,
         borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
         children: [
           SizedBox(
-            height: 72,
-            width: 86,
+            height: width,
             child: getVisual(),
           ),
-          Text(
-            text,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              height: 1.2,
-              color: Theme.of(context).colorScheme.inverseSurface,
+          FittedBox(
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                height: 1.2,
+                color: Theme.of(context).colorScheme.inverseSurface,
+              ),
             ),
           ),
           const SizedBox(height: 4),
