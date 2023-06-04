8b63a34b5a3be8cf78a149eaeb4661e527a5af0c
alifgiant <personal@alifakbar.com>
2023-06-04 11:16:18 +0800

    setup initial build

--- lib/src/service/subs_service.dart
+++ lib/src/service/subs_service.dart
@@ -1,19 +1,109 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
 
 class SubsService extends ChangeNotifier {
   SubsKind subsKind = SubsKind.none;
   bool isLoading = false;
+  final InAppPurchase storeClient;
+
+  SubsService({
+    InAppPurchase? storeClient,
+  }) : storeClient = storeClient ?? InAppPurchase.instance;
 
   bool get isPremiumUser => subsKind != SubsKind.none;
+  StreamSubscription<List<PurchaseDetails>>? _statusUpdateSubs;
+
+  void updateKind(SubsKind kind) {
+    subsKind = kind;
+    notifyListeners();
+  }
+
+  void startListening() async {
+    // feature not available on web
+    if (kIsWeb) return;
+
+    await _statusUpdateSubs?.cancel();
+    _statusUpdateSubs = storeClient.purchaseStream.listen((details) async {
+      for (var detail in details) {
+        // main error flow, propagate to all listener
+        if ([
+          PurchaseStatus.error,
+          PurchaseStatus.canceled,
+        ].contains(detail.status)) {
+          // for (var listener in _statusUpdateListeners.values) {
+          //   await listener(detail);
+          // }
+          // // remove all listeners when error occur
+          // _statusUpdateListeners.clear();
+          // continue;
+        }
+
+        // main success flow
+        // final listener = _statusUpdateListeners[detail.productID];
+        // if (listener != null) {
+        //   await listener(detail);
+        //   // remove only selected listener
+        //   _statusUpdateListeners.remove(detail.productID);
+        //   continue;
+        // }
+
+        // default/restoring flow
+        handlePurchaseStatus(detail);
+      }
+    });
+  }
+
+  Future<void> handlePurchaseStatus(PurchaseDetails event) async {
+    // if (event.pendingCompletePurchase) {
+    //   _storeClient.completePurchase(event);
+    //   return null;
+    // }
+
+    // switch (event.status) {
+    //   case PurchaseStatus.restored:
+    //   case PurchaseStatus.purchased:
+    //     if (_purchasedProducts.contains(event.productID)) break;
+    //     // add to temp data
+    //     _purchasedProducts.add(event.productID);
+    //     // save to local storage
+    //     _savePurchasedPack(event.productID);
+    //     notifyListeners();
+    //     return PaymentResult.success;
+    //   case PurchaseStatus.canceled:
+    //     return PaymentResult.cancel;
+    //   case PurchaseStatus.error:
+    //     return PaymentResult.error;
+    //   default:
+    //     //PurchaseStatus.pending:
+    //     return null;
+    // }
+    // return null;
+  }
 
   Future<void> checkSubscription() async {
     // TODO: use google service to check subs
+    restorePurchase();
   }
 
   Future<void> restorePurchase() async {
     isLoading = true;
     notifyListeners();
 
+    // feature not available on web
+    if (kIsWeb) return;
+
+    try {
+      // read from remote store
+      final available = await storeClient.isAvailable();
+      if (!available) return;
+
+      await storeClient.restorePurchases();
+    } catch (e) {
+      FlutterError.reportError(FlutterErrorDetails(exception: e));
+    }
     // TODO: use google service to subs
     await Future<void>.delayed(const Duration(seconds: 2));
     subsKind = SubsKind.monthly;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_crashlytics
 import firebase_remote_config
 import flutter_local_notifications
 import flutter_timezone
+import in_app_purchase_storekit
 import in_app_review
 import isar_flutter_libs
 import package_info_plus
@@ -27,6 +28,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
